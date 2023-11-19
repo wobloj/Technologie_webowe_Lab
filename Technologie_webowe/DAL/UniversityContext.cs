bae0189a90d6d87cd336377ec0bffc1a7f756d49
wikto <wikto@LAPTOP-8C47HQAJ>
2023-11-19 17:55:55 +0100

    Dodaj pliki projektów.

---
+++ Technologie_webowe/DAL/UniversityContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Technologie_webowe.Models;
+
+namespace Technologie_webowe.DAL
+{
+    public class UniversityContext  : DbContext
+    {
+        public UniversityContext() : base("UniversityContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrolments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Technologie_webowe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Technologie_webowe.MvcApplication" Language="C#" %>
